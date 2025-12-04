43c483ea6b6c25fd259ef3b24e2a6a7fcfe4391c
ardizanki2919 <setiawanardi2024@gmail.com>
2025-12-04 17:26:55 +0700

    feat: improve seo metadata and enable ssr

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -3,8 +3,10 @@ import { Welcome } from "../welcome/welcome";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "Ardi" },
-    { name: "description", content: "Welcome to ardizanki!" },
+    { title: "Ardi - Test Engineer" },
+    { name: "description", content: "Test Engineer based in Indonesia" },
+    { name: "keywords", content: "test, quality assurance, automation, interface, api, technology, tech, software engineering, web development, web, app, opinion" },
+    { name: "viewport", content: "width=device-width, initial-scale=1" },
   ];
 }
 
